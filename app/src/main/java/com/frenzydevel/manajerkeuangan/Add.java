9b401cddbfb612e4dcb2f0b5285268d3bfb54351
RizkyFerdiansyah28 <altama.soft@gmail.com>
2023-02-23 20:34:07 +0700

    view histori, view add data

--- app/src/main/java/com/frenzydevel/manajerkeuangan/Add.java
+++ app/src/main/java/com/frenzydevel/manajerkeuangan/Add.java
@@ -1,20 +1,29 @@
 package com.frenzydevel.manajerkeuangan;
 
+import static android.content.ContentValues.TAG;
+
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Add#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class Add extends Fragment {
 
     TextView tvadd;
@@ -26,6 +35,8 @@ public class Add extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private EditText etTanggal;
+    private DatePickerDialog.OnDateSetListener dateSetListener;
 
     public Add() {
         // Required empty public constructor
@@ -49,13 +60,18 @@ public class Add extends Fragment {
         return fragment;
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
@@ -66,15 +82,47 @@ public class Add extends Fragment {
         View v = inflater.inflate(R.layout.fragment_add, container, false);
         load(v);
         try {
-            Toast.makeText(getContext(), "ini halaman add", Toast.LENGTH_SHORT).show();;
+            Toast.makeText(getContext(), "ini halaman add", Toast.LENGTH_SHORT).show();
         }catch (Exception e){
             Toast.makeText(getContext(), e.toString(), Toast.LENGTH_SHORT).show();
         }
+
+        etTanggal = v.findViewById(R.id.iptTanggal);
+
+        etTanggal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Calendar cal = Calendar.getInstance();
+                int thn = cal.get(Calendar.YEAR);
+                int bln = cal.get(Calendar.MONTH);
+                int tgl = cal.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(getContext(), android.R.style.Theme_DeviceDefault_Dialog, dateSetListener, tgl, bln, thn);
+
+                dialog.show();
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int thn, int bln, int tgl) {
+                      bln = bln+1;
+                Log.d(TAG, "onDateSet: dd/mm/yyyy" +thn+"-"+bln+"-"+tgl);
+
+
+                String date= tgl+"-"+bln+"-"+thn;
+                etTanggal.setText(date);
+            }
+        };
 
         return v;
     }
 
     public void load(View v){
-        tvadd = v.findViewById(R.id.tvAdd);
+//        tvadd = v.findViewById(R.id.tvAdd);
+//          etTanggal = v.findViewById(R.id.iptTanggal);
+
     }
+
+
 }
